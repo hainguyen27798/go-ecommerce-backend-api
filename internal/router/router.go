ac2e6de96c4bb617d94972c0d90ae90444d5ff31
harry Nguyen <hainguyen27798@gmail.com>
2024-06-15 12:22:23 +0700

    middleware basic

--- internal/router/router.go
+++ internal/router/router.go
@@ -3,10 +3,12 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	c "github.com/hainguyen27798/go-ecommerce-backend-api.git/internal/controllers"
+	"github.com/hainguyen27798/go-ecommerce-backend-api.git/internal/middlewares"
 )
 
 func NewRouter() *gin.Engine {
 	r := gin.Default()
+	r.Use(middlewares.AuthMiddleware())
 	v1 := r.Group("/api/v1")
 	{
 		user := v1.Group("/users")
--- pkg/response/httpStatusCode.go
+++ pkg/response/httpStatusCode.go
@@ -1,11 +1,14 @@
 package response
 
 const (
-	ErrCodeSuccess      = 2001
+	ErrCodeSuccess      = 20001
 	ErrCodeParamInvalid = 2003
+
+	ErrInvalidToken = 30001
 )
 
 var ErrCodeMsg = map[int]string{
 	ErrCodeSuccess:      "success",
 	ErrCodeParamInvalid: "Param invalid",
+	ErrInvalidToken:     "Token is invalid",
 }
